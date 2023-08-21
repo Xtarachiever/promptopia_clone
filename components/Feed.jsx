704c128f75c7461e2143e91d08e25bd38fdb765e
sther <estheradeyemi796@gmail.com>
2023-08-21 16:15:00 +0100

    Worked on the layout and basic styling

---
+++ components/Feed.jsx
@@ -0,0 +1,12 @@
+"use client"
+
+const Feed = () => {
+  const handleClick = () =>{
+    return console.log('hii')
+  }
+  return (
+    <div onClick={handleClick}>Feed</div>
+  )
+}
+
+export default Feed;
