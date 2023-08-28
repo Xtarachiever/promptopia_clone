53e872658ca3dccc140daae9f546e07f36a25496
sther <estheradeyemi796@gmail.com>
2023-08-28 21:14:48 +0100

    Worked on sending a prompt to the database

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import Link from "next/link";
 import Image from "next/image";
@@ -6,12 +6,10 @@ import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  const { data:session } = useSession()
 
   const [providers, setProviders] = useState(null);
   const [toggleDropDown, setToggleDropDown] = useState(false);
-
-  console.log(toggleDropDown)
 
   useEffect(() => {
     const getSetProviders = async () => {
@@ -28,13 +26,15 @@ const Nav = () => {
           height={30}
           width={30}
           className="object-contain"
+          alt="logo"
         />
         <p className="logo_text"> promptopia</p>
       </Link>
 
+      {/* {alert(providers)} */}
       {/* Desktop Navigation */}
       <div className="sm:flex hidden">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex gap-3 md:gap-5">
             <Link href="/create-prompt" className="black_btn">
               Create Post
@@ -46,7 +46,7 @@ const Nav = () => {
 
             <Link href="/profile">
               <Image
-                src="/assets/images/logo.svg"
+                src={session?.user.image}
                 width={37}
                 height={37}
                 className="rounded-full"
@@ -72,10 +72,10 @@ const Nav = () => {
       </div>
       {/* Mobile View */}
       <div className="sm:hidden flex relative">
-        {isUserLoggedIn ? (
+        {session?.user ? (
           <div className="flex">
             <Image
-              src="/assets/images/logo.svg"
+              src={session?.user.image}
               width={37}
               height={37}
               className="rounded-full"
@@ -107,6 +107,7 @@ const Nav = () => {
                     setToggleDropDown(false);
                     signOut();
                   }}
+                  className="mt-5 w-full black_btn"
                 >
                   Sign Out
                 </button>
